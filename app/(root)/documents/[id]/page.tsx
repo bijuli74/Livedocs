1eca368e1186e183cbcc2099aa88fcc87097c636
bijuli74 <rust_bondcr007@yahoo.com>
2024-10-14 20:39:34 +0530

    added AddDocumentBtn component

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,10 +1,15 @@
+import CollaborativeRoom from "@/components/CollaborativeRoom";
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Headers";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Document = () => {
-  return <div></div>;
+  return (
+    <main className="flex w-full flex-col items-center">
+      <CollaborativeRoom />
+    </main>
+  );
 };
 
 export default Document;
