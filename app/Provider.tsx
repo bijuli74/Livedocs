6569e10645abd3a1cc6784a4de701d44f4ebc7ff
bijuli74 <rust_bondcr007@yahoo.com>
2024-10-15 19:46:35 +0530

    modified CollaborativeRoom

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,5 +1,9 @@
 "use client";
+
 import Loader from "@/components/Loader";
+import { getClerkUsers } from "@/lib/actions/user.action";
+import { useUser } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 import {
   LiveblocksProvider,
   ClientSideSuspense,
@@ -7,6 +11,8 @@ import {
 import React, { ReactNode } from "react";
 
 const Provider = ({ children }: { children: ReactNode }) => {
+  const { user: clerUser } = useUser();
+
   return (
     <LiveblocksProvider
       // publicApiKey={
@@ -14,6 +20,19 @@ const Provider = ({ children }: { children: ReactNode }) => {
       // }
 
       authEndpoint="/api/liveblocks-auth"
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsers({ userIds });
+        return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerUser?.emailAddresses[0].emailAddress!,
+          text,
+        });
+
+        return roomUsers;
+      }}
     >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -1,15 +1,17 @@
-'use client';
+"use client";
 
-import Theme from './plugins/Theme';
-import ToolbarPlugin from './plugins/ToolbarPlugin';
-import { HeadingNode } from '@lexical/rich-text';
-import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
-import { LexicalComposer } from '@lexical/react/LexicalComposer';
-import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
-import { ContentEditable } from '@lexical/react/LexicalContentEditable';
-import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
-import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary';
-import React from 'react';
+import Theme from "./plugins/Theme";
+import ToolbarPlugin from "./plugins/ToolbarPlugin";
+import { HeadingNode } from "@lexical/rich-text";
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import React from "react";
+
+import { liveblocksConfig } from "@liveblocks/react-lexical";
 
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
@@ -21,13 +23,14 @@ function Placeholder() {
 
 export function Editor() {
   const initialConfig = {
-    namespace: 'Editor',
+    namespace: "Editor",
     nodes: [HeadingNode],
     onError: (error: Error) => {
       console.error(error);
       throw error;
     },
     theme: Theme,
+    editable: true,
   };
 
   return (
